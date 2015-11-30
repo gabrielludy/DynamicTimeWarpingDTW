45650e6015ed7edfbcc3e84fac7070abc3dc4afc
gabrielludy <gabrielluizferraz@gmail.com>
2015-11-30 09:22:59 -0200

    dtw.java atualzed

--- DTW.java
+++ DTW.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 
-/*
-	verificar se os fors daqui e da main estao certos. e  descobrir como vou fazer pra desocbrir a classe a partir dos numeros da tabela. 
-	descobrir o q eh o retorno da dtw
-*/
-
 public class DTW {
 	ArrayList<Serie> qList;		//teste
 	ArrayList<Serie> sList;		//treino
@@ -30,7 +25,7 @@ public class DTW {
 				float result = calculateDTWaux(qList.get(i).getPontos(), sList.get(j).getPontos());
 
 				resAndNum.add( new ResultAndNum(result, sList.get(j).getNumClass() ) );
-//System.out.println("  result:"+result);
+
 			}	
 
 
@@ -49,7 +44,6 @@ public class DTW {
 			}
 
 			iteracoes = iteracoes+1;
-//System.out.println("hits:"+hit+" "+resAndNum.get(0).getNumClass()+" "+qList.get(i).getNumClass() + " iteracoes: "+iteracoes );
 
 		}
 System.out.println("hits:"+hit+" iteracoes:"+iteracoes );
@@ -57,7 +51,6 @@ System.out.println("hits:"+hit+" iteracoes:"+iteracoes );
 
 		System.out.println("hit percentage: " + hitPercentage);
 
-//new java.util.Scanner(System.in).nextLine();
 	}
 
 	
@@ -66,8 +59,6 @@ System.out.println("hits:"+hit+" iteracoes:"+iteracoes );
 		float[][] dtwTable;
 		float cost;
 		dtwTable = new float[qPoints.size()+1][sPoints.size()+1];
-
-		//System.out.println(Arrays.deepToString(dtwTable));
 
 		//inicia tabela
 		for(int i = 0; i <= qPoints.size(); i++){
@@ -86,15 +77,9 @@ System.out.println("hits:"+hit+" iteracoes:"+iteracoes );
 
 				dtwTable[i][j] = (qPoints.get(i-1) - sPoints.get(j-1)) * (qPoints.get(i-1) - sPoints.get(j-1)) + minimum(dtwTable[i-1][j], dtwTable[i][j-1], dtwTable[i-1][j-1]);
 
-//System.out.println("dtwTable[i][j]: "+ minimum(dtwTable[i-1][j-1], dtwTable[i][j-1], dtwTable[i-1][j]) );
-//System.out.println("minnn: "+ dtwTable[i-1][j-1]+"," +dtwTable[i][j-1]+","+ dtwTable[i-1][j]);
-//new java.util.Scanner(System.in).nextLine();
 			}
 		}
 		
-//System.out.println(Arrays.deepToString(dtwTable));		
-//System.out.println("return: "+dtwTable[qList.size()-1][sList.size()-1]+ ","+dtwTable[qList.size()][sList.size()]+","+dtwTable[1][1]);
-//new java.util.Scanner(System.in).nextLine();
 
 		return dtwTable[qPoints.size()][sPoints.size()];
 	}
